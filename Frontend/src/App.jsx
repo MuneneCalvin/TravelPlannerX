71ec8f937b5eebe288dda445c9f888627deeb37c
MuneneCalvin <munenecalvn@gmail.com>
2023-06-08 17:43:38 +0300

    Added Components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,75 +1,42 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Cruise from './Pages/Cruise';
+import Flight from './Pages/Flight';
+import Hotel from './Pages/Hotel';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
-const App = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [cruiseDetails, setCruiseDetails] = useState(null);
-
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get('https://tripadvisor16.p.rapidapi.com/api/v1/cruises/searchCruises', {
-        headers: {
-          'x-rapidapi-key': 'e3048ed846msh581428970c13ed8p1ad7fdjsn344b987e0e6b',
-          'x-rapidapi-host': 'tripadvisor16.p.rapidapi.com',
-        },
-        params: {
-          q: searchQuery,
-        },
-      });
-
-      if (response.data && response.data.results && response.data.results.length > 0) {
-        const cruise = response.data.results[0];
-        setCruiseDetails(cruise);
-      } else {
-        setCruiseDetails(null);
-      }
-    } catch (error) {
-      console.log(error);
-      setCruiseDetails(null);
-    }
-  };
+function App() {
 
   return (
-    <div className="app">
-      <header>
-        <h1>Welcome to TravelPlanner!</h1>
-        <p>Plan your dream cruise vacation with us.</p>
-      </header>
+    <>
+      <BrowserRouter>
+        {/* header */}
+        <Navbar />
+        <Routes>
+          {/* pages */}
+          {/* homepage page */}
+          <Route path='/' element={<Home />} />
+          {/* Cruise page */}
+          <Route path="/blogs" element={<Cruise />} />
+          {/* about page */}
+          <Route path="about" element={<About />} />
+          {/* Flight page */}
+          <Route path="flight" element={<Flight />} />
+          {/* Hotel page */}
+          <Route path="hotel" element={<Hotel />} />
+          {/* NotFound page */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {/* footer */}
+        <Footer />
 
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Search for a cruise destination..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
+      </BrowserRouter>
 
-      {cruiseDetails && (
-        <div className="cruise-details">
-          <h2>Cruise Details</h2>
-          <div className="cruise-card">
-            <h3>{cruiseDetails.title}</h3>
-            <p>Cruise Line: {cruiseDetails.cruiseLine.name}</p>
-            <img src={cruiseDetails.cruiseLine.logoUrl} alt={cruiseDetails.cruiseLine.name} />
-            <p>Ship: {cruiseDetails.ship.name}</p>
-            <img src={cruiseDetails.ship.imageUrl} alt={cruiseDetails.ship.name} />
-            <p>Destination: {cruiseDetails.destination.name}</p>
-            <img src={cruiseDetails.destination.imageUrl} alt={cruiseDetails.destination.name} />
-            <p>Departure Port: {cruiseDetails.portDeparture.name}</p>
-            <p>Arrival Port: {cruiseDetails.portArrival.name}</p>
-            <p>Departure Date: {cruiseDetails.sailings.results[0].departureDate}</p>
-            <p>Passenger Capacity: {cruiseDetails.passengerCapacity}</p>
-            <p>Amenities: {cruiseDetails.amenities.join(', ')}</p>
-            <p>Overall Rating: {cruiseDetails.snippets.reviewSummaryInfo.overallRatings}</p>
-            <p>Number of Reviews: {cruiseDetails.snippets.reviewSummaryInfo.numberReviews}</p>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default App;
+export default App
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav className='NavbarItems'>
+            <h1 className='navbar-logo'>TripXplorer</h1>
+
+            <ul className='nav-menu'>
+                <li>
+                    <link></link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
