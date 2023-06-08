3cd728d786c54b84a0b572e89a2d0d57bb03d649
MuneneCalvin <munenecalvn@gmail.com>
2023-06-09 00:51:40 +0300

    Added contact page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Sport from './pages/Sport';
 import Business from './pages/Business';
 import Technology from './pages/Technology';
+import Contact from './pages/Contact';
 import Notfound from './pages/Notfound';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/Sport" element={<Sport />} />
                 <Route path="/Business" element={<Business />} />
                 <Route path="/Technology" element={<Technology />} />
+                <Route path="/Contact" element={<Contact />} />
                 <Route path='*' element={<Notfound />}/>
             </Routes>
 
