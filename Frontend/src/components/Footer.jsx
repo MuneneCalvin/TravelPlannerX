c8d4f6f5cc0bd1166d5bfec1b30a8be002d9cb69
MuneneCalvin <munenecalvn@gmail.com>
2023-06-09 00:48:21 +0300

    Fixed

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,45 +2,67 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className="footer">
-            <div className="box">
-                <div className="left">
-                    <div className="categories">
-                        <p>Popular places</p>
-                    <div>
-                        <p>Sports</p>
-                    </div>
-                    <div>
-                        <p>Business</p>
-                    </div>
-                    <div>
-                        <p>Technology</p>
-                    </div>
-                    </div>
-                <div className="contactUs">
-                    <div className="contact">
-                        <p>Contact Us</p>
-                        <div>Phone No. - <span>0795756638</span></div>
-                        <div>Email - <span>munenecalvn@gmail.com</span></div>
-                        <div>Address - <span>Nairobi, Kenya</span></div>
-                    </div>
+        <footer className = "py-4">
+            <div className = "container footer-row">
+                <div className = "footer-item">
+                    <a href = "index.html" className = "site-brand">
+                        Travel<span>Xplorer</span>
+                    </a>
+                    <p className = "text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet voluptates maiores nam vitae iusto. Placeat rem sint voluptas natus exercitationem autem quod neque, odit laudantium reiciendis ipsa suscipit veritatis voluptate.</p>
+                </div>
+
+                <div className = "footer-item">
+                    <h2>Follow us on: </h2>
+                    <ul className = "social-links">
+                        <li>
+                            <a href = "#">
+                                <i className = "fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "#">
+                                <i className = "fab fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "#">
+                                <i className = "fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "#">
+                                <i className = "fab fa-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "#">
+                                <i className = "fab fa-google-plus"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className = "footer-item">
+                    <h2>Popular Places:</h2>
+                    <ul>
+                        <li><a href = "#">Thailand</a></li>
+                        <li><a href = "#">Australia</a></li>
+                        <li><a href = "#">Maldives</a></li>
+                        <li><a href = "#">Switzerland</a></li>
+                        <li><a href = "#">Germany</a></li>
+                    </ul>
+                </div>
+
+                <div className = "subscribe-form footer-item">
+                    <h2>Subscribe for Newsletter!</h2>
+                    <form className = "flex">
+                        <input type = "email" placeholder="Enter Email" className = "form-control" />
+                        <input type = "submit" className = "btn" value = "Subscribe" />
+                    </form>
                 </div>
             </div>
-            <div className="right">
-                <div className="newsletter">
-                    <p>Subscribe to Our Newsletter</p>
-                    <div className="email">
-                        <input type="email" placeholder="Enter your email address" />
-                        <button>Subscribe</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="copyrights">
-            <p>© 2023 News. All Rights Reserved.</p>
-        </div>
-    </div>
+        </footer>
     );
 }
 
-export default Footer
+export default Footer;
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,20 +3,38 @@ import './Header.css'
 
 function Header() {
     return (
-        <div className="header">
-            <div className="logo">
-                TravelXplorer
-            </div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/sport">Sports</Link></li>
-                    <li><Link to="/business">Business</Link></li>
-                    <li><Link to="/technology">Technology</Link></li>
+    <nav className = "navbar">
+        <div className = "container flex">
+            <a href = "#" className = "site-brand">
+                Travel<span>Xplorer</span>
+            </a>
+
+            <button type = "button" id = "navbar-show-btn" className = "flex">
+                <i className = "fas fa-bars"></i>
+            </button>
+            <div id = "navbar-collapse">
+                <button type = "button" id = "navbar-close-btn" className = "flex">
+                    <i className = "fas fa-times"></i>
+                </button>
+                <ul className = "navbar-nav">
+                        <li><Link to="/" className = "nav-link">Home</Link></li>
+                    <li className = "nav-item">
+                        <li><Link to="/sport" className = "nav-link">Sports</Link></li>
+                    </li>
+                    <li className = "nav-item">
+                        <li><Link to="/business" className = "nav-link">Business</Link></li>
+                    </li>
+                    <li className = "nav-item">
+                        <li><Link to="/technology" className = "nav-link">Technology</Link></li>
+                    </li>
+                    <li className = "nav-item">
+                        <a href = "contact.html" className = "nav-link">Contact</a>
+                    </li>
                 </ul>
-            </nav>
+            </div>
         </div>
+    </nav>
     )
 }
 
-export default Header
+export default Header;
