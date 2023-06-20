bd4becb527b3b388c875d32029150ba40fd202a7
MuneneCalvin <munenecalvn@gmail.com>
2023-06-20 23:27:02 +0300

    Added CORS middleware

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Enable CORS middleware
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // JWT Authentication
 app.use((req, res, next) => {
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
