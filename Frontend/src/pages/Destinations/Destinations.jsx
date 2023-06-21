bbeb0f850ddfd03c9b03c5dc14612daf4a117161
MuneneCalvin <munenecalvn@gmail.com>
2023-06-21 15:14:03 +0300

    Updated destination

--- Frontend/src/pages/Destinations/Destinations.jsx
+++ Frontend/src/pages/Destinations/Destinations.jsx
@@ -1,46 +1,73 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './Destinations.css';
+import { useState } from 'react';
 
-const Destinations = () => {
-  const [destinations, setDestinations] = useState([]);
+function Destination () {
+  const [days, setDays] = useState(0);
+  const [destination, setDestination] = useState('');
+  const [tripData, setTripData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8084/destinations');
-        setDestinations(response.data);
-      } catch (error) {
-        console.log(error);
+  const handleDaysChange = (e) => {
+    setDays(e.target.value);
+  };
+
+  const handleDestinationChange = (e) => {
+    setDestination(e.target.value);
+  };
+
+  const handlePlanTrip = async () => {
+    const url = `https://ai-trip-planner.p.rapidapi.com/?days=${days}&destination=${encodeURIComponent(destination)}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'bb5683bf3cmsh9b501cf1ec358a1p128823jsn34341b886d59',
+        'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
       }
     };
 
-    fetchData();
-  }, []);
-
-  const handleBookNow = (destinationName) => {
-    // Handle the book now action, such as redirecting to a booking page or displaying a modal
-    console.log(`Book now clicked for destination: ${destinationName}`);
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setTripData(result);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
-      {destinations.map((destination) => (
-        <div key={destination.DesName} className="destination-card">
-          <h2>{destination.DesName}</h2>
-          <p>{destination.Description}</p>
-          <img src={destination.DesImage_url} alt={destination.DesName} />
-          <p className="price">Price: ${destination.DesPrice}</p>
-          <button
-            className="book-now-button"
-            onClick={() => handleBookNow(destination.DesName)}
-          >
-            Book Now
-          </button>
+
+      <label htmlFor="destination">Destination:</label>
+      <input type="text" id="destination" value={destination} onChange={handleDestinationChange} />
+
+      <label htmlFor="days">Number of days:</label>
+      <input type="number" id="days" value={days} onChange={handleDaysChange} />
+
+      <button onClick={handlePlanTrip}>Plan Trip</button>
+
+      {tripData && (
+        <div>
+          <h2>Trip Details</h2>
+          <div className='Detail-card'>
+          <ul>
+            {tripData.plan.map((day) => (
+              <li key={day.day}>
+                <h3>Day {day.day}</h3>
+                <div className='card-activities'>
+                <ul>
+                  {day.activities.map((activity) => (
+                    <li key={activity.time}>
+                      <strong>{activity.time}:</strong> {activity.description}
+                    </li>
+                  ))}
+                </ul>
+                </div>
+              </li>
+            ))}
+          </ul>
+          </div>
         </div>
-      ))}
+      )}
     </div>
   );
-};
+}
 
-export default Destinations;
+export default Destination;
