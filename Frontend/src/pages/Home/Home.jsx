a5345ef9bc691d54b8289fe3577bd1b8972adf1a
MuneneCalvin <munenecalvn@gmail.com>
2023-06-26 16:54:25 +0300

    Updated home

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
             <div className = "container">
                 <div className = "header-title">
                     <h1>Leave Your Footprints</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus rerum maxime enim odit illum in molestias beatae doloremque, ratione optio.</p>
+                    <p>Let us be your trusted companion as you embark on your next adventure, creating memories that will last a lifetime. Welcome to our travel family.</p>
                 </div>
                 <div className = "header-form">
                     <h2>Choose your Travel location:</h2>
@@ -47,7 +47,7 @@ function Home() {
                                 Reo De Janeiro, Brazil
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">Also known as the Marvelous City, captivates visitors with its stunning landscapes, iconic landmarks like Christ the Redeemer, vibrant culture, and world-renowned Carnaval celebrations.</p>
                             </div>
                         </div>
                     </div>
@@ -60,7 +60,7 @@ function Home() {
                                 North Bondi, Australia
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">Is a coastal paradise offering pristine sandy beaches, crystal-clear waters, a laid-back atmosphere, and a vibrant beach culture that entices visitors and locals alike.</p>
                             </div>
                         </div>
                     </div>
@@ -69,11 +69,11 @@ function Home() {
                         <img src = {germany} alt = "featured place" />
                         <div className = "featured-item-content">
                             <span>
-                                <i className = "fas fa-map-marker-alt"></i>
+                                <i className = "fas fa-map-marker-alt"></i>  
                                 Berlin, Germany
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">Is a dynamic and diverse metropolis, where a rich history merges with avant-garde art, bustling nightlife, and a thriving food scene, creating an unforgettable urban experience.</p>
                             </div>
                         </div>
                     </div>Name
@@ -86,7 +86,7 @@ function Home() {
                                 Khwaeng wat arun, thailand
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">It enchants visitors with its breathtaking Wat Arun temple, adorned with intricate architectural details and offering panoramic views of the Chao Phraya River, creating a truly mesmerizing spiritual and cultural experience</p>
                             </div>
                         </div>
                     </div>
@@ -99,7 +99,7 @@ function Home() {
                                 Rome, Italy
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">The eternal city, beckons with its timeless charm, ancient ruins like the Colosseum and Roman Forum, magnificent art and architecture, and delectable cuisine, making it a must-visit destination for history enthusiasts, art lovers, and food connoisseurs alike.</p>
                             </div>
                         </div>
                     </div>
@@ -112,7 +112,7 @@ function Home() {
                                 fuvahmulah, maldives
                             </span>
                             <div>
-                                <p className = "text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed dignissimos libero soluta illum, harum amet excepturi sit?</p>
+                                <p className = "text">A hidden gem in the Indian Ocean, mesmerizes visitors with its pristine white sandy beaches, vibrant coral reefs, and unique freshwater lakes, offering an idyllic tropical paradise for those seeking tranquility and natural beauty.</p>
                             </div>
                         </div>
                     </div>
@@ -134,7 +134,7 @@ function Home() {
                             <i className = "fas fa-hotel"></i>
                         </span>
                         <h3>Luxurious Hotel</h3>
-                        <p className = "text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quo, totam repellat velit, dignissimos sequi error a minima architecto fugit nisi dolorum repellendus?</p>
+                        <p className = "text">Our comprehensive range of hotel services aims to exceed your expectations, offering impeccable amenities, attentive staff, and a seamless blend of comfort and convenience to enhance your overall experience during your stay with </p>
                         <Link to='/About' className = "btn">Read more</Link>
                     </div>
 
@@ -142,8 +142,8 @@ function Home() {
                         <span className = "services-icon">
                             <i className = "fas fa-map-marked-alt"></i>
                         </span>
-                        <h3>Trave Guide</h3>
-                        <p className = "text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quo, totam repellat velit, dignissimos sequi error a minima architecto fugit nisi dolorum repellendus?</p>
+                        <h3>Travel Guide</h3>
+                        <p className = "text">Our expert travel guides are your trusted companions, providing in-depth knowledge, insider tips, and personalized recommendations to help you navigate and uncover the hidden gems of your destination, ensuring a truly enriching and unforgettable travel experience.</p>
                         <Link to='/About' className = "btn">Read more</Link>
                     </div>
 
@@ -152,7 +152,7 @@ function Home() {
                             <i className = "fas fa-money-bill"></i>
                         </span>
                         <h3>Suitable Price</h3>
-                        <p className = "text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quo, totam repellat velit, dignissimos sequi error a minima architecto fugit nisi dolorum repellendus?</p>
+                        <p className = "text">We strive to offer competitive and affordable prices for our services, ensuring that you can enjoy exceptional quality and value without compromising on your budget, making your travel experience accessible and rewarding</p>
                         <Link to='/About' className = "btn">Read more</Link>
                     </div>
                 </div>
